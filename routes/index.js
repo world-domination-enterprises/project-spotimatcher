c537a735429ef88c74c67548a952848e484c8032
Svenja Weiler <Svenja.Weiler@gmx.de>
2019-02-12 11:43:00 +0100

    setting up the routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+//ROUTE going to our PROFILE
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+
+//ROUTE going to our RESULTS-Page
+//TODO: implement matching-function here
+router.get("/results", (req, res, next) => {
+  res.render("results");
+});
+
+//ROUTE for a users PROFILE-page
+router.get("/profile/:id", (req, res, next) => {
+  const id = req.params.id;
+  User.findById(id)
+    .then(user => {
+      console.log("User--", user);
+      res.render("profile", { user });
+    })
+    .catch(err => console.log("Error getting Userdata--", err));
 });
 
 module.exports = router;
